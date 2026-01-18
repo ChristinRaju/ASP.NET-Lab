a32c4ad3feecee9677a2472e6bc16df02e331c4f
ChristinRaju <2006christinraju@gmail.com>
2026-01-18 20:01:40 +0530

    Add files via upload

---
+++ RoutingDemoMVC/RoutingDemoMVC/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingDemoMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        [Route("Details/{id}")]
+        public IActionResult Details(int id)
+        {
+            return Content("Student ID is: " + id);
+        }
+
+        [Route("Marks/{id}/{subject}")]
+
+        public IActionResult Marks(int id, string subject)
+        {
+            return Content($"Student ID: " + id + " Subject: " + subject);
+        }
+    }
+}
